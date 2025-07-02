383531e8037f75a729be3c7ead3c4f9f279216b1
Alex <le86ksa@yandex.ru>
2025-07-02 18:42:23 +0300

    gradle

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -8,6 +8,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+//import ru.netology.nmedia.BuildConfig.BASE_URL
 import ru.netology.nmedia.dto.Post
 import java.util.concurrent.TimeUnit
 
